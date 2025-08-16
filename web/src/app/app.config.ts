d37ff5353b14a10197a87f31ed88b872f375d205
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-08-16 22:05:16 +0200

    Integrate PrimeNG with Aura theme and update Angular configuration and dependencies.

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -3,11 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+
+import Aura from '@primeuix/themes/aura'
+import {providePrimeNG} from 'primeng/config';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
